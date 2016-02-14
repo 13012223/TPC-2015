6942c3a838bb150748ceeeec002b3f0b5bcb0e31
Gareth Morgan <13012223@students.southwales.ac.uk>
2016-02-14 16:33:30 +0000

    Testing.

---
+++ Shopfloor/test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopfloor
+{
+    class test
+    {
+        private int thing = 0;
+    }
+}
